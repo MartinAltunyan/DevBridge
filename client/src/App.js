70ef04e9144d95338b55e80554e34cfd966b3abf
martalt89 <martin.altunyan@gmail.com>
2018-12-19 23:11:04 -0800

    Education form wit redux ready

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import CreateProfile from './components/create-profile/CreateProfile';
 import { clearCurrentProfile } from './actions/profileActions';
 import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
 
 
 
@@ -69,6 +70,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-education" component={AddEducation} />
               </Switch>
             </div>
             <Footer />
